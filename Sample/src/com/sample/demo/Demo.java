4655e8a164f12cf858a43ce7209dc441b778d29e
sampathkumar040 <sampathkumar040@gmail.com>
2022-11-28 21:44:21 +0530

    saved

---
+++ Sample/src/com/sample/demo/Demo.java
@@ -0,0 +1,25 @@
+package com.sample.demo;
+
+import java.util.Map;
+
+public class Demo {
+	public interface SortedMap<k,v> extends Map<k,v>{
+		
+	}
+
+	public static void main(String[] args) {
+
+		
+
+		int a=5;
+
+		 Test s=(int x)->x*x;
+
+		int result=s.calculate(a);
+		System.out.println(result);
+		}
+
+	}
+
+
+
---
+++ Sample/src/com/sample/demo/Test.java
@@ -0,0 +1,11 @@
+package com.sample.demo;
+@FunctionalInterface
+public interface Test {
+	
+	
+	
+	int calculate (int x);
+	
+	}
+
+
